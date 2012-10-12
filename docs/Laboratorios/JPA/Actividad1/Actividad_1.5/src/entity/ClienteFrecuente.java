36f7f41f763a624b147b45be48f776b928ee7f7e
Encoming <Jmmartinezro@hotmail.com>
2012-10-11 20:26:29 -0500

    Agregado hasta el punto 1.6 y Script de la Bd

---
+++ docs/Laboratorios/JPA/Actividad1/Actividad_1.5/src/entity/ClienteFrecuente.java
@@ -0,0 +1,113 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Familia Martinez
+ */
+@Entity
+@Table(name = "clientes_frecuentes")
+@NamedQueries({
+    @NamedQuery(name = "ClientesFrecuentes.findAll", query = "SELECT c FROM ClienteFrecuente c"),
+    @NamedQuery(name = "ClientesFrecuentes.findById", query = "SELECT c FROM ClienteFrecuente c WHERE c.id = :id"),
+    @NamedQuery(name = "ClientesFrecuentes.findByNumCuenta", query = "SELECT c FROM ClienteFrecuente c WHERE c.numCuenta = :numCuenta"),
+    @NamedQuery(name = "ClientesFrecuentes.findByDireccion", query = "SELECT c FROM ClienteFrecuente c WHERE c.direccion = :direccion")})
+public class ClienteFrecuente implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "NUM_CUENTA")
+    private String numCuenta;
+    @Column(name = "DIRECCION")
+    private String direccion;
+
+    @OneToOne
+    private Cliente cliente;
+
+    public ClienteFrecuente() {
+    }
+
+    public ClienteFrecuente(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ClienteFrecuente)) {
+            return false;
+        }
+        ClienteFrecuente other = (ClienteFrecuente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.ClientesFrecuentes[ id=" + id + " ]";
+    }
+
+    /**
+     * @return the cliente
+     */
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    
+}
---
+++ docs/Laboratorios/JPA/Actividad1/Actividad_1.5/src/entity/Factura.java
@@ -0,0 +1,122 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Familia Martinez
+ */
+@Entity
+@Table(name = "facturas")
+@NamedQueries({
+    @NamedQuery(name = "Facturas.findAll", query = "SELECT f FROM Factura f"),
+    @NamedQuery(name = "Facturas.findById", query = "SELECT f FROM Factura f WHERE f.id = :id"),
+    @NamedQuery(name = "Facturas.findByFecha", query = "SELECT f FROM Factura f WHERE f.fecha = :fecha")})
+public class Factura implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "FECHA")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @JoinTable(name = "facturas_productos", joinColumns = {
+        @JoinColumn(name = "Facturas_ID", referencedColumnName = "ID")}, inverseJoinColumns = {
+        @JoinColumn(name = "Productos_ID", referencedColumnName = "ID")})
+    @ManyToMany
+    private List<Producto> productosList;
+    @JoinColumn(name = "Clientes_ID", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private Cliente cliente;
+
+    public Factura() {
+    }
+
+    public Factura(Integer id) {
+        this.id = id;
+    }
+
+    public Factura(Integer id, Date fecha) {
+        this.id = id;
+        this.fecha = fecha;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public List<Producto> getProductosList() {
+        return productosList;
+    }
+
+    public void setProductosList(List<Producto> productosList) {
+        this.productosList = productosList;
+    }
+
+    public Cliente getClientesID() {
+        return cliente;
+    }
+
+    public void setClientesID(Cliente clientesID) {
+        this.cliente = clientesID;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Factura)) {
+            return false;
+        }
+        Factura other = (Factura) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Facturas[ id=" + id + " ]";
+    }
+    
+}
