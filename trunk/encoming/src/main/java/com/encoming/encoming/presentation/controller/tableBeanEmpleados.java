b2746a44e0bf1f8dc558a2ad133bd5f136493a9f
Jeisson Andrés Vergara Vargas <javergarav@unal.edu.co>
2012-10-28 21:40:16 -0500

    Modificados adminEmploye.xhtml y seeEmployees.xhtml.

--- trunk/encoming/src/main/java/com/encoming/encoming/presentation/controller/tableBeanEmpleados.java
+++ trunk/encoming/src/main/java/com/encoming/encoming/presentation/controller/tableBeanEmpleados.java
@@ -1,10 +1,17 @@
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package com.encoming.encoming.presentation.controller;
 
+import com.encoming.encoming.businesslogic.facade.AdministratorFacade;
+import com.encoming.encoming.businesslogic.facade.FacadeFactory;
+import com.encoming.encoming.businesslogic.facade.PersonFacade;
 import com.encoming.encoming.entity.Administrator;
+import com.encoming.encoming.vo.AdministratorVo;
+import com.encoming.encoming.vo.PersonVo;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -13,11 +20,25 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class tableBeanEmpleados {
 
-    List <Administrator> employees;
-    
+    List <AdministratorVo> employeesAdm;
+    ArrayList<PersonVo> employees;
+
     public tableBeanEmpleados() {
+
     }
-    public void getEmployees(){
-        
+    public ArrayList<PersonVo> getEmployees(){
+        PersonFacade personFac = FacadeFactory.getInstance().getPersonFacade();
+        AdministratorFacade adminFac = FacadeFactory.getInstance().getAdministratorFacade();  
+        employeesAdm = adminFac.getList();
+        for(AdministratorVo admVo : employeesAdm){
+            employees.add(personFac.find(admVo.getPersonidPerson()));
+        }
+        return employees;
     }
+
+    public void setEmployees(ArrayList<PersonVo> employees) {
+        this.employees = employees;
+    }
+    
+    
 }
