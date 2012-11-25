059a9e71bf2be54ce2308719fe79fb63f537086c
aetuttal <>
2012-11-25 00:39:40 -0500

    Se hizo la aserción para cuando se intenta ingresar un curson con el nombre Prueba0

--- source/final/Final/src/com/encoming/controller/CursoController.java
+++ source/final/Final/src/com/encoming/controller/CursoController.java
@@ -20,7 +20,7 @@ public class CursoController {
     public static String validateName(String text) {
         int a = text.length();
         if (a<4 || a>25) {
-            return "La longitud del nombre debe estar entre 4 y 15";
+            return "La longitud del nombre debe estar entre 4 y 25";
         }
         return "Validado";
     }
